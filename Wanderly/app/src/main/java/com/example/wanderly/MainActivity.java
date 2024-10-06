6dac51d10bb928064500e605dc1f361d242f78ee
提小莫 <76027786+qiting2270@users.noreply.github.com>
2024-10-07 02:34:29 +1100

    navigate to sign up page, back button

--- Wanderly/app/src/main/java/com/example/wanderly/MainActivity.java
+++ Wanderly/app/src/main/java/com/example/wanderly/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,12 +26,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });*/
 
-        //navigate to login layout.
+        //navigate to Login page.
         Button loginWithEmailButton = (Button) findViewById(R.id.button_loginWithEmail);
         loginWithEmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        //navigate to Signup page
+        TextView createAccountButton = (TextView) findViewById(R.id.createAccount);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
---
+++ Wanderly/app/src/main/java/com/example/wanderly/SignupActivity.java
@@ -0,0 +1,30 @@
+package com.example.wanderly;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        //back icon logic
+        ImageView backIcon = (ImageView) findViewById(R.id.back_icon);
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go back to previous activity
+                finish();
+            }
+        });
+
+    }
+}
