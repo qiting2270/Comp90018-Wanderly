911715496d6f2957b3a1f85cbc81479ec0a3bde9
提小莫 <76027786+qiting2270@users.noreply.github.com>
2024-10-07 02:00:47 +1100

    login layout, connect 2 layout

---
+++ Wanderly/app/src/main/java/com/example/wanderly/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.wanderly;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //back icon logic
+        ImageView backIcon = (ImageView) findViewById(R.id.back_icon);
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go back to previous activity
+                finish();
+            }
+        });
+
+
+    }
+}
