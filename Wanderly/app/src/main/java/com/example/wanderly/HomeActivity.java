076b9144162805d7b872a5d464213ddbcf1c3362
提小莫 <76027786+qiting2270@users.noreply.github.com>
2024-10-10 17:22:29 +1100

    home page display greeting when log in

--- Wanderly/app/src/main/java/com/example/wanderly/HomeActivity.java
+++ Wanderly/app/src/main/java/com/example/wanderly/HomeActivity.java
@@ -1,14 +1,29 @@
 package com.example.wanderly;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private String currentUserId;
+    private TextView home_greeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +31,32 @@ public class HomeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
+        auth = FirebaseAuth.getInstance();
+        currentUserId = auth.getCurrentUser().getUid();
+        home_greeting = findViewById(R.id.home_greeting);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User Information");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String userId = snapshot.child("user_id").getValue(String.class);
+
+                    if (userId != null && userId.equals(currentUserId)) {
+                        // If the user_id matches the currentUserId, get last name
+                        String lastName = snapshot.child("lastname").getValue(String.class);
+                        home_greeting.setText("Bon Voyage, " + lastName + "!");
+                        break; // Exit the loop once the user is found
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
