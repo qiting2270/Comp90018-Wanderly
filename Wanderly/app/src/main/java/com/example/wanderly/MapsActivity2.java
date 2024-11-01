5bc52deb4eb7009dfcea312122827205bf4fe6f3
提小莫 <76027786+qiting2270@users.noreply.github.com>
2024-11-01 16:56:59 +1100

    change map UI

--- Wanderly/app/src/main/java/com/example/wanderly/MapsActivity2.java
+++ Wanderly/app/src/main/java/com/example/wanderly/MapsActivity2.java
@@ -41,18 +41,9 @@ public class MapsActivity2 extends AppCompatActivity implements OnMapReadyCallba
 
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationClient;
-    private EditText latitudeInput;
-    private EditText longitudeInput;
-    private Button showStreetViewButton;
-    Intent intent;
-    private double latitude;
-    private double longitude;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
-    LatLng targetLatLng; //11
-
-    HashMap<String, double[]> locationMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,48 +88,6 @@ public class MapsActivity2 extends AppCompatActivity implements OnMapReadyCallba
             }
         });
 
-
-
-
-        locationMap.put("Thai Town", new double[]{-33.8784, 151.2070});
-        locationMap.put("Billy's Central", new double[]{-37.8136, 144.9631});
-        locationMap.put("Bornga", new double[]{-37.8140, 144.9660});
-        locationMap.put("Sweet Canteen", new double[]{-37.8162, 144.9613});
-        locationMap.put("National Gallery of Victoria", new double[]{-37.8226, 144.9690});
-        locationMap.put("State Library of Victoria", new double[]{-37.8099, 144.9656});
-        locationMap.put("Queen Victoria Market", new double[]{-37.8076, 144.9568});
-        locationMap.put("Old Melbourne Gaol", new double[]{-37.8060, 144.9654});
-
-        intent = getIntent();
-// 通过 key"Location" 获取传递的值
-        String location = intent.getStringExtra( "location");
-        Log.d("MAPS2","My location is " + location);
-
-        if (location != null && locationMap.containsKey(location)) {
-            // 获取该 location 对应的经纬度
-            double[] coordinates = locationMap.get(location);
-            if (coordinates != null) {
-                latitude = coordinates[0];
-                longitude = coordinates[1];
-                targetLatLng = new LatLng(coordinates[0], coordinates[1]);
-                Log.d("MapsActivity2", "here with targetlocation" + targetLatLng);
-
-
-            }
-        } else {
-            System.out.println("Location not found in locationMap.");
-        }
-
-
-        //binding = ActivityMaps2Binding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
-
-
-        // 初始化输入框和按钮
-        latitudeInput = findViewById(R.id.latitude_input);
-        longitudeInput = findViewById(R.id.longitude_input);
-        showStreetViewButton = findViewById(R.id.show_street_view_button);
-
         // 获取位置服务客户端
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -146,30 +95,6 @@ public class MapsActivity2 extends AppCompatActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-        // 设置显示街景按钮的点击事件
-        showStreetViewButton.setOnClickListener(v -> {
-            String latStr = latitudeInput.getText().toString();
-            String lonStr = longitudeInput.getText().toString();
-            if (!latStr.isEmpty() && !lonStr.isEmpty()) {
-                try {
-                    double latitude = Double.parseDouble(latStr);
-                    double longitude = Double.parseDouble(lonStr);
-                    LatLng latLng = new LatLng(latitude, longitude);
-                    updateLocation(latLng);
-                } catch (NumberFormatException e) {
-                    Toast.makeText(MapsActivity2.this, "Invalid latitude or longitude", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(MapsActivity2.this, "Please enter both latitude and longitude", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-
-
-
 
     }
 
@@ -190,18 +115,10 @@ public class MapsActivity2 extends AppCompatActivity implements OnMapReadyCallba
             // 请求权限
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
-        //mMap.setPadding(0, 1700, 0, 250);
+        mMap.setPadding(0, 2200, 0, 300);
 
 
     }
-
-    // 根据用户输入的经纬度更新位置
-    private void updateLocation(LatLng latLng) {
-        mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Selected Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
-    }
-
 
     private void getDeviceLocation() {
         try {
